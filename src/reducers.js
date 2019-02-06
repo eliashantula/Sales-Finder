5764340b46ee12750983794a2447e57183c1c644
Elias Hantula <ehantula@indiana.edu>
2019-02-05 19:41:27 -0500

    archive

--- src/reducers.js
+++ src/reducers.js
@@ -6,8 +6,9 @@ const initialState = {
 	dairy: [],
 	drinks: [],
 	recipes: [],
-	list: []
-};
+	list: {},
+	total: 0,
+	amount: 0};
 
 export default function groceryItem(state = initialState, Action) {
 	
@@ -34,23 +35,61 @@ export default function groceryItem(state = initialState, Action) {
 			};
 		case "ADD_SHOPPING":
 		
-		    let items = parseInt(Action.data)
-		    console.log(Action.data)
-			for (let i = 0; i < state.list.length;i++) {
-			if (state.list[i].item === Action.data.item){
-				state.list[i].amount += items
+		    let amount = parseInt(Action.data.amount)
+		    
+		    let product = Action.data.item
+		    let company = Action.data.company
+		    let price = parseFloat(Action.data.price)
+		    
+		   if (amount) {
+
+		   
+			if (state.list[product]) {
 				return {
+					...state,
+					total: state.total + (amount*price),
+					amount: state.amount + amount,
+					list : {
+						...state.list,
+						[product]: {
+							...state.list[product],
+							amount: state.list[product].amount+ amount
+						}
+
+
+
+					}
+					
+			}
+			} else {
+				console.log(state.total)
+				return {
+
 				...state,
-				list: state.list
+				total: state.total + (amount*price),
+				amount: state.amount + amount,
+				list : {
+					...state.list,
+					[product] : {product: product, amount: amount, company: company}
+
 				}
+
+				}
+
 				
 			}
-
 			}
-			return {
-			...state,
-			list: [...state.list, {item: Action.data.item, amount:items}]
-		};
+			
+				return {
+				...state,
+				
+
+
+				}
+				
+			
+
+			
 		
 		case "REMOVE_SHOPPING":
 		for (let i =0; i < state.list.length; i++){
