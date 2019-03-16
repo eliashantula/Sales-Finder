4ce9a62bac2064a901a0c1539de11d95915dd34c
Elias Hantula <ehantula@indiana.edu>
2019-03-16 13:08:27 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -13,41 +13,83 @@ import { Jumbotron, Button } from "reactstrap";
 import { getCookies } from "./actions";
 import foodmoney from "./foodmoney.jpg";
 import beta from "./beta-stamp.png";
-import Login from './login'
+import Login from "./login";
 
-const home = props => {
-  return (
-    <div>
-      <Jumbotron className="jumbo" style={{ marginBottom: "0px" }}>
-   
-        <h3 className="display-4">Hello, Sales!</h3>
-        
-        <div>
-          <img src={beta} alt="beta" />
-        </div>
-        <p className="lead">
-          Choose your items and check out some money saving recipes
-        </p>
-        <hr className="my-2" />
+class home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { login: false, register: false };
+  }
 
-        <p className="lead">
-          <Button color="primary">
-            Select Stores <i>(Coming soon)</i>
-          </Button>
-        </p>
-      </Jumbotron>
-      <div
-        className="introBack"
-        style={{
-          height: "790px",
-          backgroundImage: `url(${foodmoney})`,
-          backgroundSize: "cover"
-        }}
-      />
-    </div>
-  );
-};
+  loginClick = e => {
+    this.setState({ login: true });
+  };
 
+  registerClick = e => {
+    this.setState(prevState=>({
+      register: !prevState.register
+    }))
+  };
+
+  render() {
+    return (
+      <div>
+        <Jumbotron
+          className="jumbo"
+          style={{ marginBottom: "0px", padding: "0px" }}
+        >
+          <h3 className="display-4" style={{ paddingTop: "0px" }}>
+            Hello, Sales!
+          </h3>
+
+          <div>
+            {this.state.register === true ? <Login /> :
+            <img src={beta} alt="beta" />}
+          </div>
+          <p className="lead">
+            Find recipes and items on sale to create a shopping list which
+            maximizes savings!
+          </p>
+          <hr className="my-2" />
+
+          <p
+            className="lead"
+            style={{ marginBottom: "0px", paddingBottom: "9px" }}
+          >
+            <Button color="primary" style={{ fontSize: "12px" }}>
+              Select Stores <i>(Coming soon)</i>
+            </Button>
+            <Button
+              onClick={this.registerClick}
+              color="white"
+              style={{ fontSize: "12px", backgroundColor: "white" }}
+            >
+              Sign Up
+            </Button>
+            <Button
+              color="white"
+              style={{
+                fontSize: "12px",
+                backgroundColor: "black",
+                color: "white"
+              }}
+            >
+              Login
+            </Button>
+          </p>
+        </Jumbotron>
+        <div
+          className="introBack"
+          style={{
+            height: "790px",
+            backgroundImage: `url(${foodmoney})`,
+            backgroundSize: "cover"
+          }}
+        />
+      </div>
+    );
+  }
+}
 /*<div className="content" style={{ backgroundImage: `url(${picture})`, height: "1300px", padding: "0%"}}>*/
 
 class App extends Component {
@@ -68,7 +110,6 @@ class App extends Component {
 
           <Switch>
             <Route exact path="/" component={home} />
-            <Route exact path ="/login" component = {Login} />
             <Route exact path="/meats" component={MeatContainer} />
             <Route exact path="/vegetables" component={VegetableContainer} />
             <Route exact path="/dairy" component={DairyContainer} />
