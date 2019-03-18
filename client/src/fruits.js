1823c0110aee6030e058d67fadd9d5548c5d94d7
Elias Hantula <ehantula@indiana.edu>
2019-03-18 18:22:13 -0400

    upload

--- client/src/fruits.js
+++ client/src/fruits.js
@@ -3,13 +3,10 @@ import { Container, Row, Button, Form } from "reactstrap";
 import ItemInfo from "./itemInfo";
 import fruit from "./fruit.jpg";
 
-
 export default class Fruits extends Component {
-	
 	render() {
-		
-		const { items, onClick,name } = this.props;
-	
+		const { items, onClick, name } = this.props;
+
 		return (
 			<div
 				className="carn"
@@ -18,13 +15,20 @@ export default class Fruits extends Component {
 					width: "100%",
 					height: "1340px"
 				}}
-			>   
+			>
 				<Container className="product">
-			
-				<Button style={{backGroundColor: "black", color: "black", fontSize: "8px" }} id={name} onClick	={onClick}>
-				Sort by price
-				</Button>
-		
+					<Button
+            style={{
+              backGroundColor: "black",
+              color: "black",
+              fontSize: "8px"
+            }}
+            id={name}
+            onClick={onClick}
+          >
+            Sort by price
+          </Button>
+
 					<Row>
 						{items.map((item, i) => {
 							return <ItemInfo item={item} key={i} />;
